6e43be598db9eccd99faf643162cdb9bab9f2a7d
saphess <saphess@mail.ru>
2025-01-09 02:12:59 +0300

    add tests and service generate data

---
+++ src/test/java/DateChangeTest.java
@@ -0,0 +1,69 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DateChangeTest {
+    static String locale;
+
+    @BeforeAll
+    static void setUp() {
+        locale = "ru";
+        Configuration.browser = "chrome";
+    }
+
+    @BeforeEach
+    void open() {
+        Selenide.open("http://localhost:9999");
+    }
+
+    @Test
+    public void sendFormAndChangeDate() {
+        var gen = new GenerateData();
+        String firstDate = gen.generateDate(5);
+        String secondDate = gen.generateDate(8);
+
+        $("[data-test-id='city'] [type='text']").setValue(gen.generateCity(locale)); //город
+
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME),
+                Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").setValue(firstDate); //дата
+
+        $("[data-test-id='name'] [type='text']").setValue(gen.generateName(locale)); //имя
+
+        $("[data-test-id='phone'] [type='tel']").setValue(gen.generatePhone(locale)); //телефон
+
+        $("[data-test-id='agreement']").click(); //галочка
+
+        $$("button").findBy(Condition.text("Запланировать")).click(); // Запланировать
+
+        $("[data-test-id='success-notification']").shouldBe(Condition.visible).
+                shouldHave(Condition.text("Встреча успешно запланирована на " + firstDate),
+                        Condition.text("Успешно!")); // первое уведомление
+
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME),
+                Keys.BACK_SPACE);
+        $("[data-test-id='date'] .input__control").setValue(secondDate); //изменение даты
+
+        $$("button").findBy(Condition.text("Запланировать")).click(); // Запланировать
+
+        $("[data-test-id='replan-notification']").shouldBe(Condition.visible).
+                shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"),
+                        Condition.text("Необходимо подтверждение")); // второе уведомление
+
+        $$("button").findBy(Condition.text("Перепланировать")).click(); //Перепланировать
+
+        $("[data-test-id='success-notification']").shouldBe(Condition.visible).
+                shouldHave(Condition.text("Встреча успешно запланирована на " + secondDate),
+                        Condition.text("Успешно!")); // первое уведомление
+    }
+
+}
---
+++ src/test/java/GenerateData.java
@@ -0,0 +1,29 @@
+import com.github.javafaker.Faker;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+@Data
+public class GenerateData {
+    public String generateDate(int days) {
+        String pattern = "dd.MM.yyyy";
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    public String generateCity(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.address().city();
+    }
+
+    public String generateName(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+
+    public String generatePhone(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+}
