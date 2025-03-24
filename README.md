08b54c6d6d49345816ddbb705b341169d1fe048e
Maria <saphess@mail.ru>
2025-03-24 18:43:25 +0300

    Allure added

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # patterns [![Build status](https://ci.appveyor.com/api/projects/status/kraccfrniklv0ige?svg=true)](https://ci.appveyor.com/project/saphess/patterns)
 
 Задание 1
+
+Скриншоты из Allure
+![img.png](img.png)
+![img_1.png](img_1.png)
--- src/test/java/ru/netology/delivery/test/DateChangeTest.java
+++ src/test/java/ru/netology/delivery/test/DateChangeTest.java
@@ -2,6 +2,10 @@ package ru.netology.delivery.test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -14,9 +18,19 @@ import ru.netology.delivery.data.DataGenerator;
 public class DateChangeTest {
     static String locale = "ru";
 
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
     @BeforeEach
     void open() {
         Selenide.open("http://localhost:9999");
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
     @Test
